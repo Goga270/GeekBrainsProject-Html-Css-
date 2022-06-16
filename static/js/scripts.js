1a015c8c797c38e71e20f229caf9c00dc6f549e8
Goga270 <90174259+Goga270@users.noreply.github.com>
2022-06-17 01:25:18 +0300

    Выгрузка дз 4

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+let url = window.location.pathname;
 
 //Menu SCRIPTS
 let menuImg = document.querySelector('.menu_img');
@@ -9,8 +9,25 @@ let darkScene = document.querySelectorAll('.brandDark');
 let menu = document.querySelector('.menu');
 let content = document.querySelector('.brand');
 let offers = document.querySelector('.offersWrap');
-let darkImgFetured = document.querySelector('.feturedCardImgDark');
-let feturedCard = document.querySelector('.feturedCard');
+
+if(url.includes('index.html')){
+    content.addEventListener('click', function (){
+        for (let i=0;i<darkScene.length;i++){
+            darkScene[i].classList.add('hidden');
+        }
+        //darkScene.classList.add('hidden');
+        menu.classList.add('hidden');
+    });
+
+    offers.addEventListener('click', function (){
+        for (let i=0;i<darkScene.length;i++){
+            darkScene[i].classList.add('hidden');
+        }
+        //darkScene.classList.add('hidden');
+        menu.classList.add('hidden');
+    });
+}
+
 menuImg.addEventListener('click', function (){
     for (let i=0;i<darkScene.length;i++){
         darkScene[i].classList.toggle('hidden');
@@ -25,24 +42,53 @@ menuCloseImg.addEventListener('click', function (){
     //darkScene.classList.add('hidden');
     menu.classList.add('hidden');
 });
-content.addEventListener('click', function (){
-    for (let i=0;i<darkScene.length;i++){
-        darkScene[i].classList.add('hidden');
-    }
-    //darkScene.classList.add('hidden');
-    menu.classList.add('hidden');
-});
 
-offers.addEventListener('click', function (){
-    for (let i=0;i<darkScene.length;i++){
-        darkScene[i].classList.add('hidden');
-    }
-    //darkScene.classList.add('hidden');
-    menu.classList.add('hidden');
-});
-feturedCard.addEventListener('mousemove', function (){
-   //darkImgFetured.classList.toggle('hidden')
-});
+//FILTERS scripts
+
+let filterLabel = document.querySelector('.filterLabel');
+let filterPopup = document.querySelector('.filterPopup');
+let filterIcon = document.querySelector('.filterIcon');
+let filterPopupCategoryTitle = document.querySelectorAll('.filterPopupCategoryTitle');
+let filterSizeCheckboxes = document.querySelector('.filterSizeCheckboxes');
+let filterSizeWrap = document.querySelector('.filterSizeWrap');
+let filterTrendsWrap = document.querySelector('.filterTrendsWrap');
+let filterPriceWrap = document.querySelector('.filterPriceWrap');
+let filterTrendsCheckboxes = document.querySelector('.filterTrendsCheckboxes');
+let filterPriceCheckboxes = document.querySelector('.filterPriceCheckboxes');
+let filterPrice = document.querySelector('.filterPrice');
+
+if(url.includes('catalog.html')){
+    filterLabel.addEventListener('click', function (){
+        filterPopup.classList.toggle('hidden');
+        filterLabel.classList.toggle('filterPopupLabelHover');
+        filterIcon.classList.toggle('filterPopupIconHover');
+
+        if(filterIcon.getAttribute('src') === "static/images/catalog/filters/filter.svg"){
+            filterIcon.setAttribute('src', "static/images/catalog/filters/filterHover.svg");
+        }else {
+            filterIcon.setAttribute('src', "static/images/catalog/filters/filter.svg");
+        }
+    });
+
+    filterPopupCategoryTitle.forEach(function (catTitle){
+       catTitle.addEventListener('click', function (event){
+           event.target.nextElementSibling.classList.toggle('hidden');// nextElementSibling - даёт нам ближайшего соседа справа для элемента filterpopupCategoryTittle
+       });
+    });
+
+    filterSizeWrap.addEventListener('click', function (){
+       filterSizeCheckboxes.classList.toggle('hidden');
+    });
+
+    filterTrendsWrap.addEventListener('click', function (){
+        filterTrendsCheckboxes.classList.toggle('hidden');
+    });
+
+    filterPriceWrap.addEventListener('click', function (){
+        filterPriceCheckboxes.classList.toggle('hidden');
+        filterPriceCheckboxes.classList.toggle('filterPriceFixedPosition');
+    });
 
 
+}
 
